61bdb355031e43d93e5c091c4035a3fabd90f0ed
abdelawani <abdelawani@users.noreply.github.com>
2025-05-04 08:42:16 -0500

    Update llm_wrapper.py

--- llm_wrapper.py
+++ llm_wrapper.py
@@ -1,6 +1,7 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
+# temperature=0 for deterministic, model_name can be "gpt-4" if you have access
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
 def get_answer(docs, query):
